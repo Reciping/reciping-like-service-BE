7ea5bc6d461bfd91cd4c4bb1e9021ce28faee075
beginninggrace <tldms123455@naver.com>
2025-05-05 01:17:38 +0900

    :tada: first commit

---
+++ src/main/java/com/three/recipinglikeservicebe/RecipingLikeServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.three.recipinglikeservicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingLikeServiceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipingLikeServiceBeApplication.class, args);
+    }
+
+}
